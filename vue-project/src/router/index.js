2967815454f447e8790297838f269fccfd12578e
winnie2346 <winniedong68@gmail.com>
2026-02-09 12:32:36 -0500

    asdasd

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import one from '@/views/one.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: one,
+    },
+  ],
 })
 
 export default router
--- vue-project/src/views/one.vue
+++ vue-project/src/views/one.vue
@@ -1,7 +1,6 @@
-<script setup></script>
 <template>
   <div>
-    <RouterView />
+    <h1>Pizza Pizza Pizza</h1>
   </div>
 </template>
 
