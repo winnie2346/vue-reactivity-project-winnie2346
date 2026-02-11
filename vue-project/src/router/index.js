5aae28d31a07359b9f0ad790ccf5cc997bba33ad
winnie2346 <winniedong68@gmail.com>
2026-02-11 12:31:36 -0500

    aSsSAs

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import one from '@/views/one.vue'
+import hello from '@/views/hello.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/hi',
       name: 'home',
+      component: hello,
+    },
+    {
+      path: '/game',
+      name: 'game',
       component: one,
     },
   ],
--- vue-project/src/views/one.vue
+++ vue-project/src/views/one.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-    <h1>Pizza Pizza Pizza</h1>
-    <img src="/crust.png" alt="">
+    <div>
+      <h1>Chef Bob's 3 Mitchellin stor Pizzaria</h1>
+      <h2>NO REFUNDS</h2>
+      <h3>a dolloar a slice, no complaints</h3>
+    </div>
+
+    <img src="/chefbob.png" alt="" />
+    <img src="/chee.png" alt="" />
+    <img src="/sauce.png" alt="" />
+    <img src="/ham.png" alt="" />
+    <img src="/pnapple.png" alt="" />
+    <img src="/halopeno.png" alt="" />
   </div>
 </template>
 
